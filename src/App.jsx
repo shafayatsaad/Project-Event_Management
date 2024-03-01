744354f23369e3dbff9c2fe9a5cfa9d632215aae
Shafayat Saad <shafayat.saad30@gmail.com>
2024-03-02 00:13:56 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { createBrowserRouter, RouterProvider }
+import EventDetails from "./pages/EventDetails/EventDetails";
+import FilterEvents from "./pages/FilterEvents/FilterEvents";
+import EventList from "./pages/EventList/EventList";
+
+
 import "./App.css";
+
+constant router = createBrowserRouter([
+  {path:"/",element:<EventList/>},
+])
 
 function App() {
   return <></>;
