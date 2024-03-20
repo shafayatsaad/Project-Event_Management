825a2ea5e948fe5bd14f6a6176bec0a91d5350c7
Fahiiiim <160669812+Fahiiiim@users.noreply.github.com>
2024-03-20 21:00:00 +0600

    a
    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./routes/routes";
+import EventList from "./pages/EventList/EventList";
+import FilterEvents from "./pages/FilterEvents/FilterEvents";
+import EventDetail from "./pages/EventDetails/EventDetails";
+
 import "./App.css";
 
-const router = createBrowserRouter(routes);
+const routes = createBrowserRouter([
+  { path: "/", element: <EventList /> },
+  { path: "/find-events", element: <FilterEvents /> },
+  { path: "/events/:id", element: <EventDetail /> },
+]);
+
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
