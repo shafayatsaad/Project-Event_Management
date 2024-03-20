84767a138aadf1d6bf65b48151ab88dcb430a8f4
Shafayat Saad <shafayat.saad30@gmail.com>
2024-03-20 21:18:09 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import EventDetails from "./pages/EventDetails/EventDetails";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EventDetail from "./pages/EventDetails/EventDetails";
 import FilterEvents from "./pages/FilterEvents/FilterEvents";
 import EventList from "./pages/EventList/EventList";
 
-
 import "./App.css";
 
-constant router = createBrowserRouter([
-  {path:"/",element:<EventList/>},
-  {path:"/find-events",element:<FilterEvents/>},
-  {path:"/events/:id",element}
-])
+const router = createBrowserRouter([
+  { path: "/", element: <EventList /> },
+  { path: "/find-events", element: <FilterEvents /> },
+  { path: "/events/:id", element: <EventDetail /> },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/EventList/EventList.jsx
@@ -0,0 +1,8 @@
+const EventList = () => {
+  return (
+    <div>
+      <h1>Event List</h1>
+    </div>
+  );
+};
+export default EventList;
