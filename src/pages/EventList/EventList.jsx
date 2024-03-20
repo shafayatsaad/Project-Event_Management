3f7659ddf9b2775ad6fa5e3e84fa37dbb2fdf095
Shafayat Saad <shafayat.saad30@gmail.com>
2024-03-21 00:42:03 +0600

    update

--- src/pages/EventList/EventList.jsx
+++ src/pages/EventList/EventList.jsx
@@ -1,7 +1,34 @@
+import EventCard from "../../components/EventCard/EventCard.jsx";
+import { eventList } from "../../utils/EventDatabase.jsx";
+import Navigation from "../../components/Navigation/Navigation.jsx";
+import "./EventList.css";
 const EventList = () => {
+  const renderEventCards = () => {
+    return eventList.map(({ id, date, heading, location, img }) => {
+      return (
+        <EventCard
+          key={id}
+          id={id}
+          date={date}
+          heading={heading}
+          location={location}
+          img={img}
+        />
+      );
+    });
+  };
   return (
     <div>
-      <h1>Event List</h1>
+      <Navigation />
+      <div className="event-list-wrapper">
+        <div className="event-list">
+          {eventList.length > 0 ? (
+            renderEventCards()
+          ) : (
+            <p>No events available</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/FilterEvents/FilterEvents.jsx
+++ src/pages/FilterEvents/FilterEvents.jsx
@@ -1,7 +1,24 @@
+import { useCallback, useState } from "react";
+import FilterBox from "../../components/FilterBox/FilterBox";
+import SearchEventList from "../../components/SerachEventList/SearchEventList";
+import Navigation from "../../components/Navigation/Navigation";
+import "./FilterEvents.css";
 const FilterEvents = () => {
+  const [monthYear, setMonthYear] = useState({
+    selectedMonth: null,
+    selectedYear: null,
+  });
+  const getMonthYear = useCallback((selectedMonth, selectedYear) => {
+    setMonthYear({ selectedYear, selectedMonth });
+  }, []);
+
   return (
     <div>
-      <h1>Filter Events</h1>
+      <Navigation />
+      <div className="find-events-wrapper">
+        <FilterBox getMonthYear={getMonthYear} />
+        <SearchEventList monthYear={monthYear} />
+      </div>
     </div>
   );
 };
