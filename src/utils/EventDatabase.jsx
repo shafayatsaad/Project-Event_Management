c0014f8029be248d471c9e035bd0fb0d82222786
Fahiiiim <160669812+Fahiiiim@users.noreply.github.com>
2024-03-21 02:33:46 +0600

    update
    update

--- src/utils/EventDatabase.jsx
+++ src/utils/EventDatabase.jsx
@@ -2,10 +2,10 @@ export const eventList = [
   {
     id: 1,
     heading: "Programming Competition",
-    date: { year: 2023, month: "July" },
+    date: { year: 2024, month: "July" },
     location: "Dhaka, Bangladesh",
 
-    img: "https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29taWNzfGVufDB8fDB8fHww",
+    img: "https://lh3.googleusercontent.com/pw/AP1GczMGbsN5IBucT2E4lqzwqZm_a7fn372FI94v7CLpCtGLgFDo7BbKw2uJsGcl3eEXL6iejUUKqynbZc5i3gcp8K73yirOvxdSjkeg8Y2hOb61WpXtsSLCdFpQ5eRiO8D70-ULB8fWYXqroKP17viIAsLsAQ=w1080-h720-s-no?authuser=0",
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ export const eventList = [
   {
     id: 3,
     heading: "Cosplay Carnival",
-    date: { year: 2023, month: "September" },
+    date: { year: 2024, month: "September" },
     location: "Dhaka, Bangladesh",
 
     img: "https://images.unsplash.com/photo-1502136969935-8d8eef54d77b?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -34,9 +34,9 @@ export const eventList = [
   {
     id: 5,
     heading: "Pohela Boishak",
-    date: { year: 2023, month: "January" },
+    date: { year: 2024, month: "April" },
     location: "Dhaka, Bangladesh",
 
-    img: "https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29taWNzfGVufDB8fDB8fHww",
+    img: "https://img.freepik.com/free-vector/hand-drawn-bengali-new-year-illustration_23-2148870133.jpg?w=826&t=st=1710966663~exp=1710967263~hmac=a459b678be81043c0c9e21cd3afba50f4ad3b7ad1d9aacb07aae6db5c4719cbe",
   },
 ];
